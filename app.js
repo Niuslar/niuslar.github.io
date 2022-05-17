7e5731cab2e8c1319df22b5b58e9e1d6fef1d6a7
Niuslar <niuslar@outlook.com>
2022-05-17 20:12:35 +0100

    feature: add multiple projects and right column

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const expandsMore = document.querySelectorAll('[expand-more]')
+
+    function expand(){
+        const showContent = document.getElementById(this.dataset.target)
+        if(showContent.classList.contains('expand-active')) {
+            this.innerHTML=this.dataset.showtext
+        }
+        else{
+            this.innerHTML=this.dataset.hidetext
+        }
+        showContent.classList.toggle('expand-active')
+    }
+    expandsMore.forEach(expandMore => {
+        expandMore.addEventListener('click', expand)
+    })
+})
